544ca5ea57e2eeaafd2faf777b1a0e722cd2aef5
Марина Донцова <dontsovam@mail.ru>
2022-10-28 19:52:55 +0300

    Написала программу решения задач

--- Program.cs
+++ Program.cs
@@ -7,3 +7,79 @@
 // ["hello","2","world",":-)"]->["2", ":-)"] 
 // ["1234","1567","-2","computer science"]->["-2"]
 // ["Russia","Denmark","Kazan"]->[]
+
+// метод ввода числа 
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// метод создания и заполнения пользователем исходного массива
+string[] GetArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива:");
+        array[i] = Console.ReadLine() ?? "";
+    }
+    return array;
+}
+
+// метод для определения размера нового массива newArray
+int SizeArray(string[] arr, int сharacters)
+{
+    int size = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int length = arr[i].Length;
+        if (length <= сharacters)
+        {
+            size++;
+        }
+    }
+    return size;
+}
+
+// метод создания и заполнения нового массива из строк, длина которых меньше либо равна simbol.
+string[] GetResultArray(string[] arr, int size, int сharacters)
+{
+    string[] newArray = new string[size];
+    int indexNewArray = 0;
+    for (int indexArray = 0; indexArray < arr.Length; indexArray++)
+    {
+        int length = arr[indexArray].Length;
+        if (length <= сharacters)
+        {
+            newArray[indexNewArray] = arr[indexArray];
+            indexNewArray++;
+        }
+    }
+    return newArray;
+}
+
+// метод вывода массива в терминал
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"'{array[i]}' ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int sizeArray = ReadNumber("Введите размер создаваемого массива:");
+string[] array = GetArray(sizeArray);
+int simbol = ReadNumber("Введите максимальную длину строки для элементов нового массива:");
+Console.WriteLine("Массив пользователя:");
+PrintArray(array);
+int sizeNewArray = SizeArray(array, simbol);
+Console.WriteLine(sizeNewArray);
+string[] newArray = GetResultArray(array, sizeNewArray, simbol);
+Console.WriteLine("Полученный массив:");
+PrintArray(newArray);
